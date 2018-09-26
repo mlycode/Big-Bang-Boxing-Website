9f1e33fcc0faecbfa754dc7c07707d7c51183e49
mlycode <m.ly@live.co.uk>
2018-09-26 14:56:22 +0700

    create subtitle component

---
+++ src/components/Subtitle/Subtitle.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./Subtitle.css";
+
+const subtitle = (props) => (
+    <h2 className="Subtitle">{props.children}</h2>
+);
+
+export default subtitle;
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 
 import Title from "../../components/Title";
+import Subtitle from "../../components/Subtitle";
 
 class Home extends Component {
     render () {
         return (
-            <Title>Big Bang Boxing</Title>
+            <React.Fragment>
+                <Title>Big Bang Boxing</Title>
+                <Subtitle>Located in Jeongja, South Korea!</Subtitle>
+            </React.Fragment>
         )
     }
 };
