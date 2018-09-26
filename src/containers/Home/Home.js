c67754b6def2a67fadec951c21e6deb0d3d0790b
mlycode <m.ly@live.co.uk>
2018-09-26 15:19:20 +0700

    add logo to home page with link to Facebook

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -2,13 +2,25 @@ import React, { Component } from "react";
 
 import Title from "../../components/Title";
 import Subtitle from "../../components/Subtitle";
+import logo from "../../assets/images/logosmall.jpg";
 
 class Home extends Component {
     render () {
+        const logoStyle = {
+            borderRadius: "50%",
+            width: "260px",
+            height: "260px",
+            border: "4px solid white",
+            marginTop: "40px",
+            padding: "0"
+        }
         return (
             <React.Fragment>
                 <Title>Big Bang Boxing</Title>
                 <Subtitle>Located in Jeongja, South Korea!</Subtitle>
+                <a href="https://www.facebook.com/bigbangboxing/" target="blank">
+                    <img style={logoStyle} src={logo} alt="Logo"/>
+                </a>
             </React.Fragment>
         )
     }
