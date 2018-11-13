57ba6e3624736945c0c89dbce69e00f905c8bc22
mlycode <m.ly@live.co.uk>
2018-11-13 19:20:49 +0000

    embed facebook page

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -27,30 +27,37 @@ class Home extends Component {
                                 <div className="homePageHeaderContent">
                                     <h1>Welcome to Big Bang Boxing</h1>
                                     <h3>Canadian Owned and Operated, Located in Jeongja, South Korea!</h3>
-                                    <Paragraph
-                                        paragraphs={welcomeText} />
                                 </div>
                             </Col>
                         </Row>
                     </Grid>
                 </div>
                 <Grid className="grid-content">
+                    <CardWithImage cardImage={coachJoeImg} imageLeft>
+                        <h1>About Us</h1>
+                    </CardWithImage>
+                    <Row>
+                        <Col xs={12}>
+                            <Paragraph
+                                paragraphs={welcomeText} />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={4}></Col>
+                        <Col md={4}><div class="fb-page" data-href="https://www.facebook.com/bigbangboxing/" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false"><blockquote cite="https://www.facebook.com/bigbangboxing/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/bigbangboxing/">Big Bang Boxing 빅뱅복싱</a></blockquote></div></Col>
+                        <Col md={4}></Col>
+                    </Row>
                     <CardWithImage cardImage={coachJoeImg}>
                         <h1>Our Coaches</h1>
                     </CardWithImage>
                     <CoachMessage thumbnailImage={coachJoeImg}>
                         <h3>Coach Joe - Head Coach and Owner</h3>
                         <Paragraph paragraphs={coachMessageText} />
-                    </CoachMessage>
-                    <CardWithImage cardImage={coachJoeImg} imageLeft>
                         <Quote
                             quoteBody="Anyone can learn how to box. It's the best physical exercise that there is. People want to have fun while working out! they want to train martial arts! They want to get in better shape! Here, we do all of these things!"
                             quoteName="Coach Joe"
                         />
-                    </CardWithImage>
-                    <CardWithImage cardImage={coachJoeImg}>
-                        <h1>What People Say About Us</h1>
-                    </CardWithImage>
+                    </CoachMessage>
                 </Grid>
                 
             </React.Fragment>
