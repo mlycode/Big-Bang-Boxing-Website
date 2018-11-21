f7403291e943fde0416c1cd222852ca604b94dc5
mlycode <m.ly@live.co.uk>
2018-11-21 22:53:52 +0000

    edit navbar style

--- src/components/BSNavbar/Navbar.js
+++ src/components/BSNavbar/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navbar, Nav, NavItem} from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import logo from "../../assets/images/logosmall.jpg";
 
 import "./Navbar.css";
 
@@ -8,26 +9,26 @@ const NavigationBar = (props) => (
     <Navbar collapseOnSelect fixedTop>
   <Navbar.Header>
     <Navbar.Brand>
-      <a href="/">Big Bang Boxing</a>
+      <a href="/"><img className="navbar-logo" src={logo}/></a>
     </Navbar.Brand>
     <Navbar.Toggle />
   </Navbar.Header>
   <Navbar.Collapse>
     <Nav pullRight>
       <LinkContainer exact to="/">
-        <NavItem eventKey={1}>Home</NavItem>
+        <NavItem eventKey={1}>HOME</NavItem>
       </LinkContainer>
       <LinkContainer to="/about">
-        <NavItem eventKey={2}>About us</NavItem>
+        <NavItem eventKey={2}>ABOUT US</NavItem>
       </LinkContainer>
       <LinkContainer to="/gallery">
-        <NavItem eventKey={3}>Gallery</NavItem>
+        <NavItem eventKey={3}>GALLERY</NavItem>
       </LinkContainer>
       <LinkContainer to="/contact">
-        <NavItem eventKey={4}>Contact us</NavItem>
+        <NavItem eventKey={4}>CONTACT US</NavItem>
       </LinkContainer>
       <LinkContainer to="/location">
-        <NavItem eventKey={5}>Location</NavItem>
+        <NavItem eventKey={5}>LOCATION</NavItem>
       </LinkContainer>
     </Nav>
   </Navbar.Collapse>
